f34fe81d6748a8e41f690cf738ec4119804b2f29
skshomron <sks.phenix@hotmail.com>
2020-08-10 16:32:53 -0400

    sks tests

--- src/sscce.js
+++ src/sscce.js
@@ -2,7 +2,7 @@
 
 // Require the necessary things from Sequelize
 const { Sequelize, Op, Model, DataTypes } = require('sequelize');
-
+//me
 // This function should be used instead of `new Sequelize()`.
 // It applies the config for your SSCCE to work on CI.
 const createSequelizeInstance = require('./utils/create-sequelize-instance');
